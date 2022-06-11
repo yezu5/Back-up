3ec20aef3b05b776991a067dbc8e6e2d2d967ee9
yezu5 <4261@holbertonschool.com>
2022-06-11 03:44:53 +0000

    Debugging

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - < causes an infinite loop
 * Return: 0
 */
 
@@ -13,7 +13,7 @@ int main(void)
 
 	i = 0;
 
-	while (i > 10)
+	while (i < 10)
 	{
 	 putchar(i);
 	}
